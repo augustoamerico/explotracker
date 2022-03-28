197912401dabbc730c5a70e3510da52d64c8b3d9
augustoamerico <santos.tmd@gmail.com>
2022-03-28 13:36:35 +0100

    setting new package name

--- xtore/s3_fs/s3fs_explotracker.py
+++ xtore/s3_fs/s3fs_explotracker.py
@@ -3,5 +3,5 @@ from xtore.explotracker import ExperimentStorage
 
 class S3ExperimentStorage(ExperimentStorage):
 
-    def __init__(self, s3_bucket, s3_path):
-        super().__init__(S3FileSystem(anon=False), f"/{s3_bucket}/{s3_path}/")
+    def __init__(self, s3_bucket, s3_path, kwargs={"anon": False, "default_fill_cache": False}):
+        super().__init__(S3FileSystem(**kwargs), f"/{s3_bucket}/{s3_path}/")
